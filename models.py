5cd84182b9556298fb11347c692b5ceedb5b4b46
James Comstock <jimmy.comstock21@outlook.com>
2025-08-19 22:41:30 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class NoteBase(BaseModel):
+    title: str
+    content: str
+
+class NoteCreate(NoteBase):
+    pass
+
+class NoteUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+class Note(NoteBase):
+    id: int
